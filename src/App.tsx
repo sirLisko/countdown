cfd3f08e36e6d029d77be3c77d74cecd554acb27
Luca Lischetti <luca@sirlisko.com>
2024-09-15 19:40:48 +0100

    feat: added dark theme (#20)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import CountdownPage from "./components/CountdownPage";
 import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "./components/mode-toggler";
 
-const App = () => {
-  return (
+const App = () => (
+  <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
     <div className="min-h-screen flex">
+      <div className="absolute top-4 right-4">
+        <ModeToggle />
+      </div>
       <main className="container mx-auto">
         <CountdownPage />
         <Toaster />
       </main>
     </div>
-  );
-};
+  </ThemeProvider>
+);
 
 export default App;
